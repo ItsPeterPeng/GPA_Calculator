2278a65c0d603cff3a585902fe8d6633f6a9af52
ItsPeterPeng <mpeng7379@gmail.com>
2021-01-10 17:48:09 -0500

    Initial Commit

---
+++ GPA_Calculator/GPA_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPA_CalculatorApp.swift
+//  GPA_Calculator
+//
+//  Created by Meixiong Peng on 1/10/21.
+//
+
+import SwiftUI
+
+@main
+struct GPA_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
